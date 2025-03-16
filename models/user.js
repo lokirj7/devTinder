c438e7ed2d02bffab99d73bb5ff82d98cdf74046
loki <iamloki765@gmail.com>
2025-03-17 00:01:47 +0530

    updated config

--- models/user.js
+++ models/user.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 // Like Table Schema
-const UserSchema = new mongoose.Schema ({
+const UserSchema = new mongoose.Schema (
+    {
 firstName:{
     type:String
 },
@@ -21,8 +24,30 @@ age:{
 gender:{
     type:String
 }
-});
+},
+{
+    timestamps:true
+}
 
+);
+
+// 1. jwt token sign creating token or cookie storing in brower
+UserSchema.methods.getJWT = async function (){
+const user = this ;
+const token = await jwt.sign({_id:user._id},process.env.JWT_SECRET,{expiresIn:'2h'})
+
+return token;
+}
+
+
+
+// 2. validating password from db
+UserSchema.methods.validatePassword = async function (passwordInputByUser){
+const user = this ;
+const validatedUser = await bcrypt.compare(passwordInputByUser,user.password)
+return validatedUser;
+
+}
 // ALways Collection Should Need To Be In Capital Start it's Like Table
 const User = mongoose.model("Users",UserSchema);
 
--- src/app.js
+++ src/app.js
@@ -9,32 +9,19 @@ const User = require('../models/user')
 const app = express()
 const jwt = require('jsonwebtoken');
 const {validateSignUpData} = require('../utils/validation'); 
+const {userAuth} = require("../middlewares/auth");
 app.use(express.json())
 app.use(cookieParser())
 
 
 
-app.get("/profile", async (req, res) => {
+app.get("/profile",userAuth, async (req, res) => {
     try {
-        console.log("data");
-        const cookieToken = req.cookies
-        const {token} = cookieToken;
-        console.log(token, "cookieTokens");
-
-        if (!token) {
-            return res.status(401).json({ error: "Invalid Token" }); // Proper JSON response
+        const {user} = req
+        if(!user){
+            throw new Error("Noo Such User Found");
         }
-
-        // Decoding cookie
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const { _id } = decoded;
-        
-        const user = await User.findById(_id);
-        if (!user) {
-            return res.status(404).json({ error: "No Such User Exists" }); // Proper JSON response
-        }
-
-        res.status(200).send(user); // Ensure JSON response
+        res.status(200).send(req.user); // Ensure JSON response
     } catch (error) {
         res.status(500).json({ error: error.message }); // Catch unexpected errors
     }
@@ -50,15 +37,15 @@ app.post("/login",async(req,res)=>{
         if(!user){
             throw new Error("Emailid not present in db");
         }else{
-            const isPasswordValid = await bcrypt.compare(password,user.password);
+            const isPasswordValid = await user.validatePassword(password);
 
             // 1. Creating jwt token
 
             // 2. Adding it to cookies sending back to browser
             if(isPasswordValid){
                 
-                const token = jwt.sign({_id:user._id},process.env.JWT_SECRET);
-                res.cookie('token',token);
+                const token = await user.getJWT()
+                res.cookie('token',token,{expiresIn:'2h'});
                 res.send("Login Successfully");
             }else{
                 throw new Error("Emailid not present in db");
@@ -68,41 +55,6 @@ app.post("/login",async(req,res)=>{
         res.status(400).send("Error :" +err.message);
     }
 })
-
-// app.get("/allusers",async (req,res)=>{
-//     try{
-//         const allusers = await User.find({})
-//         res.send(allusers)
-//     }catch(err){
-//         res.status(501).send("No Data Error")
-//     }
-// })
-
-// Get Specific User
-app.get("/user",async(req,res)=>{
-    const userEmail = req.body.emailId
-    try{
-        const user = await User.findOne({emailId:userEmail})
-        res.status(202).send(user)
-    }catch(err){
-        res.status(401).send(err)
-    }
-
-})
-
-// delete user in db
-// update user data
-app.patch("/user",async(req,res)=>{
-    const userId = req.body.userId;
-    const data = req.body;
-    try{
-        const user = await User.findByIdAndUpdate({_id:userId},data,{returnDocument:"after",runValidators:true});
-    }catch(err){
-        res.status(400).send("Update Failed");
-    }
-})
-
-
 
 connectDB()
 .then(()=>{
