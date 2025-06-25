41cf10931a0aed68a3dede2115e9c7788ebf36dc
loki <iamloki765@gmail.com>
2025-06-25 20:32:52 +0530

    implemented pagination for user feed endpoint

--- routes/user.js
+++ routes/user.js
@@ -15,7 +15,17 @@ const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
 userRouter.get("/feed",userAuth,async(req,res)=>{
     try{
         const loggedInUser = req.user;
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
 
+        // Ensure limit does not exceed 50
+        limit = Math.min(limit, 50);   
+        const skip = (page - 1) * limit;
+        // Validate pagination parameters
+        if (page < 1 || limit < 1) {    
+            return res.status(400).json({ error: "Invalid pagination parameters" });
+        }   
+      
         // Fetching all connection request for logged in user
         const connectionRequests = await ConnectionRequest.find({
             $or:[
@@ -24,6 +34,7 @@ userRouter.get("/feed",userAuth,async(req,res)=>{
             ]
         })
 
+        // User Has to be hide from the feed has been pushed to hiddenUserIds
         const hiddenUserIds = new Set();
         connectionRequests.forEach(request => {
                 hiddenUserIds.add(request.fromUserId.toString());
@@ -33,7 +44,10 @@ userRouter.get("/feed",userAuth,async(req,res)=>{
         // Fetching all users except the logged in user and hidden users
         const users = await User.find({
             _id: { $nin: [loggedInUser._id, ...Array.from(hiddenUserIds)] }
-        }).select(USER_SAFE_DATA);
+        })
+        .select(USER_SAFE_DATA)
+        .skip(skip)
+        .limit(limit);
 
         res.status(200).json({
             message: "Users fetched successfully",
